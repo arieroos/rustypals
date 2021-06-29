6b986950aacb41167315b2c3870b9218101b227f
Arie De Bok <arie@directdebit.co.za>
2021-06-29 16:40:33 +0200

    Start chapter 1 challenge 3

--- src/crypto_lib.rs
+++ src/crypto_lib.rs
@@ -1,5 +1,6 @@
 use hex;
 use std::error::Error;
+use std::collections::HashMap;
 
 fn hex_to_base64(hex_str: &str) -> Result<String, Box<dyn Error>> {
     Ok(base64::encode(hex::decode(hex_str)?))
@@ -17,10 +18,30 @@ fn fixed_xor_hex(hex_str_a: &str, hex_str_b: &str) -> Result<String, Box<dyn Err
     Ok(hex::encode(bytes_a))
 }
 
-fn fixed_xor(bytes_a: &mut Vec<u8>, bytes_b: &Vec<u8>) {
+pub fn fixed_xor(bytes_a: &mut Vec<u8>, bytes_b: &Vec<u8>) {
     for i in 0..bytes_a.len() {
         bytes_a[i] ^= bytes_b[i]
     }
+}
+
+pub fn try_decrypt_single_xor(decode_str: &str, attempt_with: u8) -> String {
+    let mut enc_bytes = hex::decode(decode_str).unwrap();
+    let mut frequencies = HashMap::new();
+
+    for b in enc_bytes.iter() {
+        let v = *frequencies.entry(b).or_insert(0);
+        frequencies.insert(b, v + 1);
+    }
+
+    let mut most_frequent = frequencies
+        .into_iter().max_by_key(|a| { a.1 })
+        .unwrap().0;
+
+    let xor_byte = most_frequent ^ (attempt_with);
+    let xor_bytes = vec![xor_byte; enc_bytes.len()];
+    fixed_xor(&mut enc_bytes, &xor_bytes);
+
+    enc_bytes.into_iter().map(|b| { b as char }).collect::<String>()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ extern crate simple_error;
 
 mod crypto_lib;
 
+use hex;
+use std::collections::HashMap;
+use crate::crypto_lib::try_decrypt_single_xor;
+
 fn main() {
-    println!("Hello, world!");
+    let decode_str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+
+    println!("{}", try_decrypt_single_xor(decode_str, ' ' as u8))
 }
